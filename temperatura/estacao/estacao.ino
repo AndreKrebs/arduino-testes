2c11e86e5a8511fd743b1aba5cc8cf6fd5e9e21c
André <andre.krebssilva@gmail.com>
2017-05-06 22:57:25 -0300

    Protoboard e altera

--- temperatura/estacao/estacao.ino
+++ temperatura/estacao/estacao.ino
@@ -1,10 +1,15 @@
+//#include <Adafruit_Sensor.h>
+#include <LiquidCrystal.h>
 #include <DHT.h>
 
 // Pino conectado ao pino de dados do sensor
-#define DHTPIN 8
+#define DHTPIN 11
 
 // Sensor DHT 22  (AM2302)
 #define DHTTYPE DHT22   
+
+//Define os pinos que serão utilizados para ligação ao display
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 
 // Definicoes do sensor : pino, tipo
 DHT dht(DHTPIN, DHTTYPE);
@@ -14,8 +19,15 @@ int portaLed = 10; // Porta a ser utilizada para ligar o led
 int portaLDR = A5; // Porta analógica utilizada pelo LDR
 int portaXd28 = A0; // Porta analógica sensor de solo
 
+// variaveis com dados dos sensores
+float temp = 0;
+float humid = 0;
+
 void setup() {
   Serial.begin(9600); 
+
+  //Define o número de colunas e linhas do LCD
+  lcd.begin(16, 2);
   
   pinMode(portaLed, OUTPUT); //Define a porta do Led como saída  
   pinMode(portaXd28, INPUT); // Define porta de entrada do sensor de solo
@@ -27,31 +39,37 @@ void setup() {
 }
 
 void loop() {
-  // Aguarda 2 segundos entre as medicoes
-  delay(2000);
+  
   
   // Leitura da umidade
-  float h = dht.readHumidity();
+  humid = dht.readHumidity();
   // Leitura da temperatura (Celsius)
-  float t = dht.readTemperature();
+  temp = dht.readTemperature();
 
   // Verifica se o sensor esta respondendo
-  if (isnan(h) || isnan(t))
+  if (isnan(humid) || isnan(temp))
   {
     Serial.println("Falha ao ler dados do sensor DHT !!!");
     return;
   }
-  Serial.print("-----------------------\r\n");
+
+  
+  /*Serial.println("-----------------------");
   Serial.print("Temperatura: ");
   Serial.print(t);
-  Serial.print(" C\r\n\r\n");
+  Serial.println(" C");
   
   Serial.print("Umidade: ");
   Serial.print(h);
-  Serial.print(" %\r\n\r\n");
+  Serial.println(" %");*/
 
   sensorLight();
   sensorSoilHumidity();
+
+  printLcd();
+
+  // Aguarda segundos entre as medicoes
+  delay(5000);
 
 }
 
@@ -101,3 +119,49 @@ void sensorSoilHumidity() {
   }
 } 
 
+void printLcd() {
+  //Limpa a tela
+  lcd.clear();
+
+  //Posiciona o cursor na coluna 0, linha 0;
+  lcd.setCursor(0, 0);
+  lcd.print("AR ");
+
+  // imprime a temperatura
+  lcd.setCursor(3, 0);
+  lcd.print(temp);
+  lcd.setCursor(8, 0);
+  lcd.print("C");
+
+  lcd.setCursor(10, 0);
+  lcd.print(humid);
+  lcd.setCursor(15, 0);
+  lcd.print("%");
+  
+}
+
+/*void printLcd() {
+  //Limpa a tela
+  lcd.clear();
+  //Posiciona o cursor na coluna 3, linha 0;
+  lcd.setCursor(3, 0);
+  //Envia o texto entre aspas para o LCD
+  lcd.print("KREBSCODE!");
+  lcd.setCursor(3, 1);
+  lcd.print(" LCD 16x2");
+  //delay(5000);
+   
+  //Rolagem para a esquerda
+  for (int posicao = 0; posicao < 3; posicao++)
+  {
+    lcd.scrollDisplayLeft();
+    delay(300);
+  }
+   
+  //Rolagem para a direita
+  for (int posicao = 0; posicao < 6; posicao++)
+  {
+    lcd.scrollDisplayRight();
+    delay(300);
+  }
+}*/
